57d78b36e42951038e236f641bd7258c20880f51
ajaydevz <ajaydevzzz30@gmail.com>
2025-11-13 01:06:06 +0530

    set version

--- vynzora_pro/settings.py
+++ vynzora_pro/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY =  config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['vynzora-1cyw.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['vynzora-1.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 
