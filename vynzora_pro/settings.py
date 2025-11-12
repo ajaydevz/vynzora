73c1e8d947d0ca3ad22cfb8fe0649b410dd407a3
ajaydevz <ajaydevzzz30@gmail.com>
2025-11-12 21:51:19 +0530

    setup for hosting

--- vynzora_pro/settings.py
+++ vynzora_pro/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tbme)+s(anaw1qs6(j#xe#4vqil&3t@6led*q+%k7#c+94tfbk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['vynzoraa.onrender.com','127.0.0.1']
 
