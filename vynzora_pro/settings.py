e0748329cf1b8e9836c2e7497c90a4be0a72e34b
ajaydevz <ajaydevzzz30@gmail.com>
2025-11-13 02:37:01 +0530

    add cloudinary

--- vynzora_pro/settings.py
+++ vynzora_pro/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY =  config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['vynzora-2.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['vynzora-3.onrender.com', 'localhost', '127.0.0.1']
 
 
 
