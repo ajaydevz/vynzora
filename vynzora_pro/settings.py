be03a516005db722a37dffc112064cc2607b0e02
ajaydevz <ajaydevzzz30@gmail.com>
2025-11-15 13:52:33 +0530

    fix

--- vynzora_pro/settings.py
+++ vynzora_pro/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["vynzora-4.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["vynzora-5.onrender.com", "localhost", "127.0.0.1"]
 
 # ALLOWED_HOSTS = ['*']
 
